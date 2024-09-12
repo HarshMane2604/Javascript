0e36c5e13be956dfe0fba8a1df85ed8d67c95df3
Harsh Mane <125598995+HarshMane2604@users.noreply.github.com>
2024-09-12 15:55:17 +0000

    Datatypees

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,9 @@
+console.log("Hari"); //Test
+"use strict" //treat all JS code as newer version
+let name = "HARSH"; //string data-type
+let age = "18"; //int data-type
+// null(represent emptyness) data-type standalone value
+// undefined =>
+// Symbol => unique
+console.log(typeof(18));
+console.log(typeof(null));
