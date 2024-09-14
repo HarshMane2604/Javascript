9b8703d25ef6900c86d0b7de646f063a67b98b5e
Harsh Mane <125598995+HarshMane2604@users.noreply.github.com>
2024-09-14 08:34:03 +0000

    Operation(Operators)

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,16 @@
+let value = 3;
+var negValue = -value;
+console.log(negValue);
+
+// Operation
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 2); // for power 
+// console.log(2 / 2);
+// console.log(2 % 2);
+
+let gameCounter = 100;
+++gameCounter; //prefix Increment = increments the value
+gameCounter++;//postfix Increment = increments the value
+console.log(gameCounter);
