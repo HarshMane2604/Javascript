025466e6e8453f15d6b059a164cca7f70a6dad16
Harsh Mane <125598995+HarshMane2604@users.noreply.github.com>
2024-09-24 14:04:55 +0000

    revise

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -3,13 +3,13 @@ let score = "33abc";
 //let score = "Harsh" // check the conversion of string in int;
 // output will be NaN;
 let new_score = "33";
-//  console.log(typeof score);
+console.log(typeof score);
 //  console.log(typeof new_score); 
 
 // Conversion deals with converting the datatype in another datatype;
 
 let new_data = Number(score); //here the string datatype is converting into number(integer)
-//  console.log(typeof new_data); 
+//console.log(typeof new_data); 
 
 //There is no strict check in the javascript
 
---
+++ 03_Basics/03_Arrow.js
@@ -0,0 +1,71 @@
+
+// const user = {
+//         username: "Harsh",
+//         price: 999,
+//         welcomeMessage: function(){
+//             console.log(`${this.username}, welcome to the website`);
+//         }
+// }
+// // user.welcomeMessage();
+// user.username = "Hari";
+// user.welcomeMessage();
+ 
+//console.log(this) // In this environment {} = empty object
+
+//In the browser the globle object is window object
+
+//this keyword talks about the current context that means context refers to the scope
+
+// function chai(){
+//     let username = "Harsh"
+//     console.log(this);
+// }
+
+// chai();
+
+// const chai = function(){
+//     let username = "Harsh"
+//      console.log(this.username);
+     
+// }
+// chai();//undefined
+
+
+//Declaration of arrow function
+const chai = () => {
+    let username = "Harsh"
+    //console.log(this);
+     
+}
+chai(); //{}
+
+//Arrow function
+//Explicit return
+// const addTwo = (num1, num2) => {
+//     return num1 + num2;
+// }
+//console.log(addTwo(3, 5));
+
+//Implicit return
+//When we write with curly braces we have to use return key word and VICE VERSA
+// const addTwo = (num1, num2) => num1 + num2;
+
+// const addTwo = (num1, num2) => (num1 + num2);
+const addTwo = (num1, num2) => ({username: "Harsh" });
+console.log(addTwo(3, 5));
+
+
+//IMMEDIATELY INVOKED FUNCTION EXPRESSION
+
+//() () //first bracket express the function defination and second bracket calls the function amd semicolon is compulsory 
+(function chai(){
+    //name IIFE
+    console.log(`DB connected`);
+})();
+
+//We do this because when globle scope declared sometimes it pollutes so to avoid it we use IIFE
+
+((name) => { //unamed IFFE
+    console.log(`DB Connected ${name}`);
+})("Harsh");
+
