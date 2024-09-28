e38025dcc2505cb4290532a081e3bebbfb3bdb74
Harsh Mane <125598995+HarshMane2604@users.noreply.github.com>
2024-09-28 08:48:11 +0000

    while and do-while loops

---
+++ 05_itteration/02_two.js
@@ -0,0 +1,20 @@
+let index = 0;
+while(index <= 10){
+    //console.log(`Value of index i is ${index}`);
+    index = index + 2;
+}
+
+const myArr = ['superman', 'Batman', 'flash'];
+let arr = 0
+while (arr < myArr.length) {
+    //console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+
+//DO WHILE LOOP (Kam pehle hota hai condition bad mai check hoti hai)
+//let score = 0;
+let score = 11;//Score is 11
+do {
+    console.log(`Score is ${score}`)
+    score++;
+} while (score <= 10); 
